69f18f9714c05679007c46455a5185ae2080f076
uuencode <uuencode@users.noreply.github.com>
2016-09-15 16:25:02 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,33 +1,61 @@
 # geary2txt
 
-A cmd tool to export/backup Geary email messages as TXT/EML files. Requires python or php with pdo_sqlite extension.
+A cmd tool in Python to export / backup Geary and Pantheon-mail messages as source: TXT, EML or MHT files.
 
 https://github.com/uuencode/geary2txt
 
-- this tool will NOT change anything in the Geary database
-- messages removed from Geary are NOT removed from backup
-- previously exported messages are skipped
-- messages can be displayed or imported from any email program that supports eml files
+* this tool does NOT change anything in the Geary database
+* messages removed from Geary are NOT removed from backup
+* previously exported messages are skipped
+* messages that are visible in the Geary mail list but not yet downloaded (no header+body in the database) are not processed, (info provided instead)
 
-![Alt text](/geary2txt.gif "void")
 
-## REQUIREMENTS
+![Alt text](/geary2txt.gif "screencast GIF")
 
-python or php with pdo_sqlite extension
 
 ## HOW TO INSTALL
 
-- Download and save 'geary2txt.py' OR 'geary2txt.php' AS 'geary2txt' and make it executable
-- Set 'geary_db_file' and 'dir_to_export' at the beginning of geary2txt where:
-- geary_db_file - geary database file e.g. '/home/USER/.local/share/geary/EMAIL@EMAIL.COM/geary.db'
-- dir_to_export - a folder to save messages e.g. '/home/USER/backup_emails' - no trailing slash
+* Download and save 'geary2txt.py' and make it executable
+* Set 'geary_db_file' and 'dir_to_export' at the beginning of geary2txt.py where:
+ * geary_db_file - geary database file e.g. '/home/USER/.local/share/ geary-OR-pantheon-mail /EMAIL@EMAIL.COM/geary.db'
+ * dir_to_export - a folder to save messages e.g. '/home/USER/backup_emails' - no trailing slash
+* Make sure that you have permissions to read 'geary_db_file' and permissions to create files in 'dir_to_export' 
+
 
 ## HOW TO USE
 
-Run geary2txt from terminal to see all Geary folders you can export messages from:
+Run geary2txt from terminal:
 
-`geary2txt`
+`PATH-TO/geary2txt.py`
 
-Run geary2txt from terminal with folder IDs as arguments to export messages as txt files:
+Run geary2txt.py from terminal with folder IDs as arguments to export messages as source files without prompt:
 
-`geary2txt 2 5 12`
+`PATH-TO/geary2txt.py 2 5 12`
+
+
+## WHAT TO DO WITH THE FILES
+
+* messages can be displayed or imported from any email program that supports EML/MHT files e.g. Thunderbird
+* there is a Firefox extension that opens EML and MHT files: UnMHT
+* there is a Chrome extension to open and extract EML/MHT files: MHT Viewer
+* a cmd tool called mpack extracts the content of EML/MHT files with the following command: munpack file
+* a gtk/webkit based app on GitHub called ohwgiles/wemed claims to open & edit EML files
+
+## HISTORY
+
+* version 0.2 / 2016-09-15
+
+ * fancy colors
+ * Geary folders with no messages are not displayed
+ * log: skipped, failed and processed messages + details
+ * interactive options to choose folders and display log
+ * messages are counted internally since Geary db records seem to be unreliable
+ * messages without header & body in the database are marked as 'failed' and not processed
+ * php version removed
+
+* version 0.1 / 2016-09-07 - initial release 
+
+
+## KNOWN ISSUES
+ 
+There seems to be no easy and reliable way to convert the email subject to utf8 from the various encoding methods that are used. Now geary2txt simply removes any non-ascii symbol to create the subject part of the filename.
--- geary2txt.py
+++ geary2txt.py
@@ -1,97 +1,153 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 '''
 
-geary2txt.py
+geary2txt.py 
+version 0.2 / 2016-09-15
 
-A cmd tool to export/backup Geary email messages as TXT/EML files.
+A cmd tool in Python to export / backup Geary and Pantheon-mail messages as source: TXT, EML or MHT files.
 
 https://github.com/uuencode/geary2txt
 
-- this tool will NOT change anything in the Geary database
-- messages removed from Geary are NOT removed from backup
-- previously exported messages are skipped
-- messages can be displayed or imported from any email program that supports eml files
 
-## REQUIREMENTS
+HOW TO INSTALL
 
-python 2.x
+- Make this file executable
 
-## HOW TO INSTALL
+- Set 'geary_db_file' and 'dir_to_export' below where:
 
-Make this file executable
+  geary_db_file - geary database file e.g. '/home/USER/.local/share/ geary-OR-pantheon-mail /EMAIL@EMAIL.COM/geary.db'
+  dir_to_export - a folder to save messages e.g. '/home/USER/backup_emails' - no trailing slash
 
-Set 'geary_db_file' and 'dir_to_export' below where:
+- Make sure that you have permissions to read 'geary_db_file' and permissions to create files in 'dir_to_export' 
 
-geary_db_file - geary database file e.g. '/home/USER/.local/share/geary/EMAIL@EMAIL.COM/geary.db'
-dir_to_export - a folder to save messages e.g. '/home/USER/backup_emails' - no trailing slash
 
-## HOW TO USE
+HOW TO USE
 
-Run geary2txt.py from terminal to see all Geary folders you can export messages from:
-`geary2txt.py`
+Run geary2txt.py from terminal:
+`PATH-TO/geary2txt.py`
 
-Run geary2txt.py from terminal with folder IDs as arguments to export messages as txt files:
-`geary2txt.py 2 5 12`
+Run geary2txt.py from terminal with folder IDs as arguments to export messages as source files without prompt:
+`PATH-TO/geary2txt.py 2 5 12`
 
 '''
 
 # Settings
-geary_db_file = '/home/USER/.local/share/geary/EMAIL@EMAIL.COM/geary.db';
+geary_db_file = '/home/USER/.local/share/ geary-OR-pantheon-mail /EMAIL@EMAIL.COM/geary.db';
 dir_to_export = '/home/USER/backup_emails'; # no trailing slash
-fileextension = 'txt'; # eml or txt
+fileextension = 'eml'; # eml, txt or mht or whatever you wish
+
+# ----------
 
 
-import sys, re, datetime, sqlite3, os.path
+import sys, os, re, datetime, sqlite3
+
+hr=' --------------------------------------'
+
+# colors for printing
+class clr:
+ ppl='\033[95m'
+ blu='\033[94m'
+ gre='\033[92m'
+ yel='\033[93m'
+ red='\033[91m'
+ bld='\033[1m'
+ end='\033[0m'
+
+# create nice table output + colors
+def niceprint(a,b,c,d):
+# a=string; b=spaces to align right; c=length; d=color
+# r=result; x=calc spaces (left); y=calc spaces to length
+ r=''; x=''; y=''
+ if len(a)<b: x=b-len(a); x=x*' '
+ r=x+a
+ if len(r)<c: y=c-len(r); y=y*' '
+ r=r+y
+ if d!=0: r=d+r+clr.end
+ return r
+
+# clear the screen
+os.system('clear')
 
 print ''
+print clr.bld+' geary2txt'+clr.end+' (Export/backup Geary mail messages as source files)'
+print ''
 
-# check if db file exists
-if os.path.isfile(geary_db_file) : print ' -- Database file OK: ', geary_db_file
-else : print '-- Database file does not exists, check your settings! --\n'; exit()
+# check if db file exists or die
+if os.path.isfile(geary_db_file): print clr.gre+' Database file OK: '+clr.end, geary_db_file
+else: print clr.red+' Database file does NOT exist, check your settings!\n'+clr.end; exit()
 
-# check if dir to export exists
-if os.path.isdir(dir_to_export) : print ' -- Backup directory OK: ', dir_to_export
-else : print '-- Backup directory does not exists, check your settings! --\n'; exit()
+# check if dir to export exists or die
+if os.path.isdir(dir_to_export): print clr.gre+' Backup directory OK: '+clr.end, dir_to_export
+else: print clr.red+' Backup directory does NOT exist, check your settings!\n'+clr.end; exit()
+
+# print help
+print '\n This script can be called with the IDs of the folders you want to backup messages from:'
+print clr.blu+' geary2txt.py 1 5 12'+clr.end
 
 # connect to the database
-con = sqlite3.connect(geary_db_file)
-cur = con.cursor()
+con=sqlite3.connect(geary_db_file)
+cur=con.cursor()
 
-
-# list and print all geary mail folders
-
-cur.execute("SELECT id,name,last_seen_total FROM FolderTable")
+# Geary mail folders
 
 print ''
-print 'Geary mail folders'
-print '--------------------------------------'
+print clr.yel+' Geary folders'+clr.end+' ( where messages > 0 )'
+
+print hr
+print ' id    Folder                Messages'
+print hr
+
+# put folder names in a dictionary
+gfolders={}
+cur.execute("SELECT id,name FROM FolderTable ORDER BY id")
+for row in cur:
+ gfolders[row[0]]=row[1]
+
+# list folders and count messages since Geary counting is unreliable
+cur.execute("SELECT folder_id, COUNT(message_id) FROM MessageLocationTable GROUP BY folder_id")
+
+atleastone=0
 
 for row in cur:
-    n0=str(row[0]); n1=str(row[1]); n2=str(row[2])
-    print 'ID: '+n0+' Name: '+n1+' Messages: '+n2
+ n0=niceprint(str(row[0]),2,6,0) # folder id
+ n1=gfolders[row[0]]; n1=n1[:20]; n1=niceprint(n1,0,25,0) # folder name
+ n2=niceprint(str(row[1]),5,7,clr.red+clr.bld) # message count
+ line=' '+n0+n1+n2; atleastone+=1
+ print line
 
-print '--------------------------------------'
+if atleastone<1: 
+ cur.close(); con.close()
+ print ' No folders with messages!'
+ print hr
+ print '\n Bye bye!\n'; exit()
 
+print hr
 
 # prepare SQL IN(FOLDERS) based on the passed arguments
-
+ 
 folders2process=[]
 
+# use sys.argv first
 for arg in sys.argv:
-    if arg.isdigit(): folders2process.append(arg)
+ if arg.isdigit(): folders2process.append(arg)
 
-if len(folders2process)<1 : 
-    print 'No messages processed.'
-    print 'Call this script with the IDs of the mail folders you want to backup messages from:'
-    print 'geary2txt.py 1 5 12\n'
-    cur.close(); con.close(); exit()
+# if no sys.argv show input
+if len(folders2process)<1: 
+ passids=raw_input(" Folder IDs to export messages from or (n) to exit: ")
+ passids=passids.split()
+ for entry in passids:
+  if entry.isdigit(): folders2process.append(entry)
 
-folders2sql = ','.join(folders2process)
-folders2lst = ' '.join(folders2process)
+# if still no valid arguments die
+if len(folders2process)<1:
+ print '\n No valid folder IDs.\n Bye bye!\n'; cur.close(); con.close(); exit()
 
-print 'Folder IDs to backup messages from:',folders2lst;
+folders2sql=','.join(folders2process)
+folders2lst=' '.join(folders2process)
 
+print '\n Exporting messages from folders:',folders2lst
 
 # prepare a list of messages to backup
 
@@ -99,33 +155,71 @@ message_ids=[]
 cur.execute('SELECT DISTINCT(message_id) FROM MessageLocationTable WHERE folder_id IN('+folders2sql+')')
 
 for row in cur: 
-    message_ids.append(str(row[0]))
-message_ids = ','.join(message_ids)
+ message_ids.append(str(row[0]))
+message_ids=','.join(message_ids)
 
 
 # select the messages
+cur.execute('SELECT from_field,date_time_t,subject,header,body FROM MessageTable WHERE id IN('+message_ids+')')
 
-cur.execute('SELECT from_field,date_time_t,subject,header,body FROM MessageTable WHERE id IN('+message_ids+') AND header IS NOT NULL AND body IS NOT NULL')
-
-counter=0
-
+skippedcounter=0; successcounter=0; failedocounter=0; successlog=[]; failedolog=[]
 
 # process the messages in a loop
 for row in cur: 
-# extract sender email from 'sender' field
-    ematch = re.search('([\w.-]+)@([\w.-]+)', row[0])
-    sender = ematch.group()
-# get date/time from unix timestamp of the message
-    mdatetime=datetime.datetime.fromtimestamp(row[1]).strftime('%Y-%m-%d %H:%M:%S')
-# get subject, 50 chars only
-    subject=row[2]; subject=subject[:50]
-# prepare filename: dateime | sender | subject.extension
-    mfilename=dir_to_export+'/'+mdatetime+' | '+sender+' | '+subject+'.'+fileextension
-# message: header+body
-    mfiledata=row[3]+row[4];
-# save file
-    if not os.path.isfile(mfilename) : f = open(mfilename, 'w'); f.write(mfiledata); f.close(); counter+=1
+ mfiledata=''
 
-print counter , 'messages processed...'
-print '--------------------------------------'
-cur.close(); con.close(); exit()
+ # extract sender email from 'sender' field
+ ematch=re.search('([\w.-]+)@([\w.-]+)', row[0])
+ sender=ematch.group()
+
+ # get date/time from unix timestamp of the message
+ mdatetime=datetime.datetime.fromtimestamp(row[1]).strftime('%Y-%m-%d %H:%M:%S')
+
+ # get subject, 50 chars only
+ subject=row[2]
+ subject=re.sub(r'[^a-zA-Z0-9 ]','',subject)
+ subject=subject[:50]
+
+ # prepare log entry name and filename: dateime | sender | subject.extension
+ nametolog=' '+mdatetime+' | '+sender+' | '+subject
+ mfilename=dir_to_export+'/'+mdatetime+' | '+sender+' | '+subject+'.'+fileextension
+
+ # message: header+body and save file (skip if exists)
+ if(row[3] is not None and row[4] is not None): mfiledata=row[3]+row[4]
+ else: failedocounter+=1; failedolog.append(nametolog)
+ 
+ if os.path.isfile(mfilename): skippedcounter+=1
+ elif len(mfiledata)>0: successlog.append(nametolog); f=open(mfilename, 'w'); f.write(mfiledata); f.close(); successcounter+=1
+
+# close db connection
+cur.close(); con.close()
+
+print hr
+
+# bold result numbers
+successn=niceprint(str(successcounter),3,0,clr.bld)
+skippedn=niceprint(str(skippedcounter),3,0,clr.bld)
+failedon=niceprint(str(failedocounter),3,0,clr.bld)
+
+# print result
+print ' '+successn+clr.gre+' messages processed successfully...'+clr.end
+print ' '+skippedn+clr.blu+' messages skipped (previously processed)...'+clr.end
+print ' '+failedon+clr.red+' messages failed due to a missing header or body...'+clr.end
+print hr+'\n'
+
+# if no messages in the selected folders simply die
+if(successcounter+failedocounter)<1: print ' Bye bye!\n'; exit()
+
+# show log
+showlog=raw_input(' Would like to see a log (y) or (n): ')
+if(showlog!='y'): print ' Bye bye!\n'; exit()
+
+successlog='\n'.join(successlog)
+failedolog='\n'.join(failedolog)
+
+print ''
+print clr.gre+successlog+clr.end
+print clr.red+failedolog+clr.end
+
+print ' Bye bye!\n'
+exit()
